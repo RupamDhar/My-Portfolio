4fd223fecda9e583558f15961e36e0dc8796d854
RupamDhar <rupamdhar1000@gmail.com>
2024-06-28 13:28:51 +0530

    github-access

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -10,11 +10,18 @@ const HeroSection = () => {
                     <div className="about-me-sub-title">Software Engineer</div>
                 </div>
                 <div className="about-me-desc">
-                    I am a Computer Science Engineer based in India who can design and develop websites, web applications, mobile applications as well as games. I also have experience in 3D modelling.
+                    I am a Computer Science Engineer based in India who can design and develop websites, web applications, mobile applications as well as games. Additionally, I also have experience in 3D modelling.
                 </div>
-                <a href='/cv.pdf' download="Rupam_Dhar_CV.pdf">
-                    <button className="about-me-btn">Download CV</button>
-                </a>
+                <div className="btn-container">
+                    <a href='/cv.pdf' download="Rupam_Dhar_CV.pdf">
+                        <button className="about-me-btn">Download CV</button>
+                    </a>
+                    <a href="https://github.com/RupamDhar" target='_blank' className='github-btn'>
+                        <img src="/github.svg" alt="" className='github-logo'/>
+                        <span className='github-name'>Github</span>
+                        <i className="fa-solid fa-arrow-right github-arrow"></i>
+                    </a>
+                </div>
                 <div className='gradient'></div>
             </div>
 
